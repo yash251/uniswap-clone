4aabbaf6afea8744624136e2a3aa47fcdbc7d423
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-11 16:31:23 +0530

    connect schemas

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,5 +1,7 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
+import { userSchema } from './userSchema'
+import { transactionSchema } from './transactionSchema'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
@@ -12,5 +14,7 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    userSchema,
+    transactionSchema,
   ]),
 })
