9074d6a8fcda5f7b1365428cd224ce29eda7dd09
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-12 21:31:09 +0530

    Header done

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import type { NextPage } from 'next'
+import Header from "../components/Header"
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
 }
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className={style.wrapper}>
-      <h2>Header</h2>
+      <Header />
       <h2>Main</h2>
       <h2>Transaction History</h2>
     </div>
